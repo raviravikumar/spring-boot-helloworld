20779ac9c443e235b8f91bb87134225dc1c37d11
Ravikumar, Ramanjanappa <ramanjanappa.ravikumar@optum.com>
2023-02-28 05:04:24 -0500

    yet another refactor

--- src/main/java/com/optumrx/gitaction/demo/Application.java
+++ src/main/java/com/optumrx/gitaction/demo/Application.java
@@ -1,4 +1,4 @@
-package com.knf.dev.dem;
+package com.optumrx.gitaction.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/optumrx/gitaction/demo/GreetingController.java
+++ src/main/java/com/optumrx/gitaction/demo/GreetingController.java
@@ -1,4 +1,4 @@
-package com.knf.dev.dem;
+package com.optumrx.gitaction.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
